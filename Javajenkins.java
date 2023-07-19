ea43bfe2564821415282e6d6727c9af9a3020fba
ChaBAYt69 <haythem_bach@yahoo.fr>
2023-07-19 15:02:39 +0200

    init

---
+++ Javajenkins.java
@@ -0,0 +1,10 @@
+public class Javajenkins {
+    public static void main(String[] args) {
+        System.out.println("Bonjour tout le monde !!!! ");
+        additionner(1, 2);
+    }
+
+    public static int additionner(int a, int b) {
+        return (a + b);
+    }
+}
