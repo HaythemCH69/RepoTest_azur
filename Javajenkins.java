2084d4fb64115c31fe42deb985012b096bac0fed
ChaBAYt69 <132995371+HaythemCH69@users.noreply.github.com>
2023-07-20 14:49:39 +0200

    Update Javajenkins.java
    modif code 2

--- Javajenkins.java
+++ Javajenkins.java
@@ -2,9 +2,5 @@ public class Javajenkins {
     public static void main(String[] args) {
         System.out.println(" Bonjour tout les Devopsiiii !!!! ");
         additionner(1, 2);
-    }
-
-    public static int additionner(int a, int b) {
-        return (a + b);
-    }
+   }
 }
