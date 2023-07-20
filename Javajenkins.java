b507d5332da6cb32e6ada0751a98140e0b53649f
ChaBAYt69 <132995371+HaythemCH69@users.noreply.github.com>
2023-07-20 14:44:34 +0200

    Update Javajenkins.java
    modif code

--- Javajenkins.java
+++ Javajenkins.java
@@ -1,6 +1,6 @@
 public class Javajenkins {
     public static void main(String[] args) {
-        System.out.println(" Bonjour tout les autres !!!! ");
+        System.out.println(" Bonjour tout les Devopsiiii !!!! ");
         additionner(1, 2);
     }
 
