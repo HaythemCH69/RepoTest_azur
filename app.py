b5bc941f1186d53f6f1359683133ab29df840f1c
ChaBAYt69 <132995371+HaythemCH69@users.noreply.github.com>
2023-09-25 17:05:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
 
-# Créez une instance de l'application Flask
 app = Flask(__name__)
 
-# Définissez une route de base
 @app.route('/')
 def accueil():
-    return 'Bienvenue sur mon site web !'
+    return "Bienvenue sur ma page web !!!"
 
-# Exécutez l'application Flask
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
